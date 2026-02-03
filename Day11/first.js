2cd805b4d81e589c91ccd7fa916eade2823190f6
alensomaxx <alensocreation@gmail.com>
2026-02-03 11:43:18 +0530

    Add first.js with basic functions and string manipulations; create second.js as a placeholder

---
+++ Day11/first.js
@@ -0,0 +1,22 @@
+function fun(){
+    console.log("Hello World");
+}
+
+function sum(a,b){
+    console.log(a+b);
+}
+
+sum(5,10);
+sum(20,30);
+
+let s="javascript";
+console.log(s.length);
+console.log(s[0]);
+console.log(s[s.length-1]);
+console.log(s.indexOf('a'));
+console.log(s.lastIndexOf('a'));
+console.log(s.slice(4,10));
+console.log(s);
+console.log(s.toUpperCase());
+console.log(s.toLowerCase());
+fun();
