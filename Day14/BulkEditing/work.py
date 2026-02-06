499e0402488d1ac0cc227e605457163497014049
alensomaxx <alensocreation@gmail.com>
2026-02-06 16:48:33 +0530

    ##Code to change bulk images to black and white

---
+++ Day14/BulkEditing/work.py
@@ -0,0 +1,14 @@
+import os
+import cv2 
+
+##Code to change bulk images to black and white
+
+ip="images"
+op="bw_images"
+os.makedirs(op,exist_ok=True)
+
+
+for i in os.listdir(ip):
+    img=cv2.imread(os.path.join(ip,i))
+    gry=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    cv2.imwrite(os.path.join(op,i),gry)
