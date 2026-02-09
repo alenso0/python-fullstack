01bc3612da927f929c13139350d7a82adf23e25b
alensomaxx <alensocreation@gmail.com>
2026-02-09 14:36:03 +0530

    break-after

--- Day15/wsgi.py
+++ Day15/wsgi.py
@@ -1,6 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todoe.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=True)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+with app.app_context():
+    db.create_all()
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
 
 @app.route('/')
 def hello_world():
